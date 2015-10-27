8f36cfb4dca818f80123d68fc654c36a3851ea35
antonmry <ayuste@optaresolutions.com>
2015-10-27 20:23:42 +0100

    Spock added but with failed test #15

---
+++ src/test/groovy/TestingUnitSpec.groovy
@@ -0,0 +1,15 @@
+/**
+ *
+ * Created by antonmry on 27/10/15.
+ */
+import org.gradle.sample.Greeter;
+
+class TestingUnitSpec extends spock.lang.Specification {
+    def "testingUnit-greeter"() {
+        given: "a new TestingUnit class is created"
+        def greeter = new Greeter();
+
+        expect: "Adding two numbers to return the sum"
+        greeter.getGreeting().equals("hello Gradle - fail")
+    }
+}

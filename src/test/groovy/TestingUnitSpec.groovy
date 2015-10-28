958ce581ba616ca9aba461ec15bcab9867b2b10c
antonmry <ayuste@optaresolutions.com>
2015-10-28 08:46:39 +0100

    Test #17

--- src/test/groovy/TestingUnitSpec.groovy
+++ src/test/groovy/TestingUnitSpec.groovy
@@ -10,6 +10,6 @@ class TestingUnitSpec extends spock.lang.Specification {
         def greeter = new Greeter();
 
         expect: "Adding two numbers to return the sum"
-        greeter.getGreeting().equals("hello Gradle 2")
+        greeter.getGreeting().equals("hello Gradle")
     }
 }
